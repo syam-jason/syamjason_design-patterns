94bbba59f7604655c67826416414cb8926a64843
syam jason <syam jason@LAPTOP-2EESBBRI>
2020-03-09 23:30:51 +0530

    done

---
+++ syamjason_design-patterns/src/main/java/mediator/chatroom.java
@@ -0,0 +1,23 @@
+package mediator;
+
+import java.util.Date;
+
+public class chatroom {
+	public static void showMessage(User user, String message){
+
+		   System.out.println(new Date().toString() + " [" + user.getName() + "] : " + message);
+
+	      
+
+	   }
+
+
+
+
+
+
+
+
+
+	}
+
---
+++ syamjason_design-patterns/src/main/java/mediator/mediator.java
@@ -0,0 +1,20 @@
+package mediator;
+
+public class mediator {
+	 public static void main(String[] args) {
+
+	      User robert = new User("Robert");
+
+	      User john = new User("John");
+
+
+
+	      robert.sendMessage("Welcome to Epam!");
+
+	      john.sendMessage("syam!");
+
+	   }
+
+	}
+
+
